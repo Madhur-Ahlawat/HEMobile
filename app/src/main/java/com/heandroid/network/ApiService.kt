070235b61280f895e9e088db5a8d0e8854400592
Abhilasha.chauhan <abhilasha.chauhan@conduent.com>
2021-09-02 14:43:00 +0530

    committed

--- app/src/main/java/com/heandroid/network/ApiService.kt
+++ app/src/main/java/com/heandroid/network/ApiService.kt
@@ -1,8 +1,6 @@
 package com.heandroid.network
 
 import com.heandroid.model.*
-import okhttp3.ResponseBody
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.*
 
--- app/src/main/java/com/heandroid/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/heandroid/viewmodel/LoginViewModel.kt
@@ -6,14 +6,9 @@ import com.heandroid.network.ApiHelper
 import com.heandroid.repo.AppRepository
 import com.heandroid.repo.Resource
 import kotlinx.coroutines.Dispatchers
-<<<<<<< HEAD
-//
-//class LoginViewModel(private val appRepository: AppRepository):ViewModel() {
-class LoginViewModel(private val apiHelper: ApiHelper):ViewModel() {
-=======
 
-class LoginViewModel(private val appRepository: ApiHelper):ViewModel() {
->>>>>>> 8fce408b1dd05766b77505525f996d347d6ea88d
+class LoginViewModel(private val apiHelper: ApiHelper):ViewModel() {
+
 
     fun loginUser(clientID:String,
                   grantType:String,
@@ -24,11 +19,8 @@ class LoginViewModel(private val appRepository: ApiHelper):ViewModel() {
                   validatePasswordCompliance:String) = liveData(Dispatchers.IO) {
         emit(Resource.loading(data = null))
         try {
-<<<<<<< HEAD
             emit(Resource.success(data = apiHelper.loginApiCall(clientID, grantType, agencyId, clientSecret, value, password, validatePasswordCompliance)))
-=======
-            emit(Resource.success(data = appRepository.loginApiCall(clientID, grantType, agencyId, clientSecret, value, password, validatePasswordCompliance)))
->>>>>>> 8fce408b1dd05766b77505525f996d347d6ea88d
+
         } catch (exception: Exception) {
             emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
         }
