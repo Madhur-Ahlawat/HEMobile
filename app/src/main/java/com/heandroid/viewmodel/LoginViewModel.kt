da706870bcb9e25d08db5e0a73e092bad7475eed
Abhilasha.chauhan <abhilasha.chauhan@conduent.com>
2021-09-02 11:29:20 +0530

    committed before taking pull from master for unit testing

--- app/src/main/java/com/heandroid/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/heandroid/viewmodel/LoginViewModel.kt
@@ -2,11 +2,13 @@ package com.heandroid.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.heandroid.network.ApiHelper
 import com.heandroid.repo.AppRepository
 import com.heandroid.repo.Resource
 import kotlinx.coroutines.Dispatchers
-
-class LoginViewModel(private val appRepository: AppRepository):ViewModel() {
+//
+//class LoginViewModel(private val appRepository: AppRepository):ViewModel() {
+class LoginViewModel(private val apiHelper: ApiHelper):ViewModel() {
 
     fun loginUser(clientID:String,
                   grantType:String,
@@ -17,7 +19,7 @@ class LoginViewModel(private val appRepository: AppRepository):ViewModel() {
                   validatePasswordCompliance:String) = liveData(Dispatchers.IO) {
         emit(Resource.loading(data = null))
         try {
-            emit(Resource.success(data = appRepository.loginUser(clientID, grantType, agencyId, clientSecret, value, password, validatePasswordCompliance)))
+            emit(Resource.success(data = apiHelper.loginApiCall(clientID, grantType, agencyId, clientSecret, value, password, validatePasswordCompliance)))
         } catch (exception: Exception) {
             emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
         }
@@ -27,7 +29,7 @@ class LoginViewModel(private val appRepository: AppRepository):ViewModel() {
                               refreshToken:String, validatePasswordCompliance:String) = liveData(Dispatchers.IO) {
         emit(Resource.loading(data = null))
         try {
-            emit(Resource.success(data = appRepository.getRenewalAccessToken(clientId , grantType, agencyId, clientSecret, refreshToken!!, validatePasswordCompliance)))
+            emit(Resource.success(data = apiHelper.getRenewalAccessToken(clientId , grantType, agencyId, clientSecret, refreshToken!!, validatePasswordCompliance)))
         } catch (exception: Exception) {
             emit(Resource.error(data = null, message = exception.message ?: "Error Occurred!"))
         }
