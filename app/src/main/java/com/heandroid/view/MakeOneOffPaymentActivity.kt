1fce41493e6ba8a9f49adebd1edb420d8a4ecba4
Abhilasha.chauhan <abhilasha.chauhan@conduent.com>
2021-12-07 13:33:29 +0530

    working on email confirmation flow . Committed by : Abhilasha Chauhan

---
+++ app/src/main/java/com/heandroid/view/MakeOneOffPaymentActivity.kt
@@ -0,0 +1,18 @@
+package com.heandroid.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.heandroid.R
+import com.heandroid.databinding.ActivityMakeOneOffPaymentBinding
+
+
+private lateinit var dataBinding: ActivityMakeOneOffPaymentBinding
+
+class MakeOneOffPaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_make_one_off_payment)
+    }
+}
