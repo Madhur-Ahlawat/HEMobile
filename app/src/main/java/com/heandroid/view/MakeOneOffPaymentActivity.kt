ec9dcabe92c26a02bc15f80ea539ede717b9ca7d
Abhilasha.chauhan <abhilasha.chauhan@conduent.com>
2021-12-08 18:08:44 +0530

    What is : Committed after fixing build failed issue. Done by : Abhilasha Chauhan

--- app/src/main/java/com/heandroid/view/MakeOneOffPaymentActivity.kt
+++ app/src/main/java/com/heandroid/view/MakeOneOffPaymentActivity.kt
@@ -4,12 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.heandroid.R
-import com.heandroid.databinding.ActivityMakeOneOffPaymentBinding
-
-
-private lateinit var dataBinding: ActivityMakeOneOffPaymentBinding
+import com.heandroid.databinding.FragmentMakeOneOffPaymentBinding
 
 class MakeOneOffPaymentActivity : AppCompatActivity() {
+
+    private lateinit var dataBinding: FragmentMakeOneOffPaymentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
