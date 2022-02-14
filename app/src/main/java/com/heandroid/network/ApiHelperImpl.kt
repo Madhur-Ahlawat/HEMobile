a2cd906991bfa740dd9c469c2ae374b334e66862
cherukuri.pench <cherukuri.prasad@conduent.com>
2022-02-14 18:42:41 +0530

    LogOut api Implemented and Custom dialog like ios Implemented

--- app/src/main/java/com/heandroid/network/ApiHelperImpl.kt
+++ app/src/main/java/com/heandroid/network/ApiHelperImpl.kt
@@ -23,6 +23,8 @@ class ApiHelperImpl(private val apiService: ApiService) : ApiHelper {
         validatePasswordCompliance
     )
 
+    override suspend fun getLogOut()= apiService.logOut()
+
     override suspend fun getAccountOverviewApiCall(authToken: String) =
         apiService.getAccountOverview(authToken)
 
