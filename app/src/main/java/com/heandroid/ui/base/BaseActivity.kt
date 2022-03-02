04495b8ab84079c8e8a5770ac4bfd3e1bbf9b36b
Tanvir Ahmed <tanvir.ahmed@conduent.com>
2022-03-02 15:49:31 +0530

    session expire flow added and login test cases

--- app/src/main/java/com/heandroid/ui/base/BaseActivity.kt
+++ app/src/main/java/com/heandroid/ui/base/BaseActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.heandroid.utils.logout.LogoutListner
-import com.heandroid.utils.logout.LogoutUtil
 
 abstract class BaseActivity<T> : AppCompatActivity() {
 
