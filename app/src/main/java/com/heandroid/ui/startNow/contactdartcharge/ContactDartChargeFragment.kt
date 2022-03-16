2d7b03717e3f3ff07c3cfcbf0ec92dd5384d2d94
TejaSalana <salana.rao@conduent.com>
2022-03-16 14:08:38 +0530

    contact dart charge

--- app/src/main/java/com/heandroid/ui/startNow/contactdartcharge/ContactDartChargeFragment.kt
+++ app/src/main/java/com/heandroid/ui/startNow/contactdartcharge/ContactDartChargeFragment.kt
@@ -2,12 +2,11 @@ package com.heandroid.ui.startNow.contactdartcharge
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.heandroid.R
-import com.heandroid.databinding.FragmentAboutServiceBinding
 import com.heandroid.databinding.FragmentContactDartChargeBinding
 import com.heandroid.ui.base.BaseFragment
 import com.heandroid.utils.extn.customToolbar
-import com.heandroid.utils.extn.toolbar
 
 class ContactDartChargeFragment : BaseFragment<FragmentContactDartChargeBinding>() {
     override fun getFragmentBinding(
@@ -15,14 +14,15 @@ class ContactDartChargeFragment : BaseFragment<FragmentContactDartChargeBinding>
         container: ViewGroup?
     ) = FragmentContactDartChargeBinding.inflate(inflater, container, false)
 
-    override fun init() {}
-
-    override fun onResume() {
-        super.onResume()
+    override fun init() {
         requireActivity().customToolbar(getString(R.string.str_contact_dart_charge))
     }
 
-    override fun initCtrl() {}
+    override fun initCtrl() {
+        binding.makeEnquiry.setOnClickListener {
+            findNavController().navigate(R.id.action_contactDartCharge_to_dartChargeAccountTypeSelectionFragment)
+        }
+    }
 
     override fun observer() {}
 }
---
+++ app/src/main/java/com/heandroid/ui/startNow/contactdartcharge/DartChargeAccountTypeSelectionFragment.kt
@@ -0,0 +1,76 @@
+package com.heandroid.ui.startNow.contactdartcharge
+
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextUtils
+import android.text.style.StyleSpan
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
+import androidx.navigation.fragment.findNavController
+import com.heandroid.R
+import com.heandroid.data.model.account.AccountTypeSelectionModel
+import com.heandroid.databinding.FragmentAccountTypeSelectionBinding
+import com.heandroid.databinding.FragmentDartChargeAccountTypeSelectionBinding
+import com.heandroid.ui.base.BaseFragment
+import com.heandroid.utils.common.Constants
+import com.heandroid.utils.extn.*
+
+class DartChargeAccountTypeSelectionFragment : BaseFragment<FragmentDartChargeAccountTypeSelectionBinding>(),
+    View.OnClickListener,
+    RadioGroup.OnCheckedChangeListener {
+    private lateinit var accountModel: AccountTypeSelectionModel
+    private var accountSelection: String = Constants.MAIN_ACCOUNT
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) =  FragmentDartChargeAccountTypeSelectionBinding.inflate(inflater, container, false)
+
+
+    override fun init() {
+        requireActivity().customToolbar(getString(R.string.cases_and_enquiry))
+        accountModel = AccountTypeSelectionModel()
+    }
+
+    override fun initCtrl() {
+        binding.apply {
+            model = accountModel
+            btnContinue.setOnClickListener(this@DartChargeAccountTypeSelectionFragment)
+            rgAccount.setOnCheckedChangeListener(this@DartChargeAccountTypeSelectionFragment)
+        }
+    }
+
+    override fun observer() { }
+
+    override fun onCheckedChanged(rg: RadioGroup?, checkedId: Int) {
+        when (checkedId) {
+            R.id.yes -> {
+              binding.model = AccountTypeSelectionModel(true)
+            }
+            R.id.no -> {
+                binding.model = AccountTypeSelectionModel(true)
+            }
+        }
+    }
+
+
+    override fun onClick(view: View?) {
+        view?.let {
+            when (it.id) {
+                R.id.btnContinue -> {
+                    if (binding.yes.isChecked){
+                        requireContext().showToast("yes")
+//                        findNavController()
+                    } else {
+//                        findNavController()
+                        requireContext().showToast("no")
+                    }
+                }
+                else -> { }
+            }
+        }
+    }
+
+}
