d1d3d879f7477bd6f7d1bf25fe478d8b13ef1ab6
52364682 <shivam.gupta@conduent.com>
2023-03-16 18:37:13 +0530

    Account Creation Design

---
+++ app/src/main/java/com/conduent/nationalhighways/ui/account/creation/CreateAccountEmailVerificationFragment.kt
@@ -0,0 +1,26 @@
+package com.conduent.nationalhighways.ui.account.creation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.conduent.nationalhighways.R
+
+
+class CreateAccountEmailVerificationFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(
+            R.layout.fragment_create_account_email_verificationfragment,
+            container,
+            false
+        )
+    }
+
+}
---
+++ app/src/main/java/com/conduent/nationalhighways/ui/account/creation/CreateAccountResendCodeFragment.kt
@@ -0,0 +1,30 @@
+package com.conduent.nationalhighways.ui.account.creation
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.conduent.nationalhighways.databinding.FragmentCreateAccountResendCodeBinding
+import com.conduent.nationalhighways.ui.base.BaseFragment
+
+
+class CreateAccountResendCodeFragment : BaseFragment<FragmentCreateAccountResendCodeBinding>(), View.OnClickListener {
+
+
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentCreateAccountResendCodeBinding = FragmentCreateAccountResendCodeBinding.inflate(inflater, container, false)
+    override fun init() {
+    }
+
+    override fun initCtrl() {
+    }
+
+    override fun observer() {
+    }
+
+    override fun onClick(v: View?) {
+    }
+
+}
