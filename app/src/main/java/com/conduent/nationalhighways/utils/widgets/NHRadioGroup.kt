138ce5f6096db16eabfcacac1fb7069d9fd8ea7b
52364682 <shivam.gupta@conduent.com>
2023-05-22 12:47:34 +0530

    account creation screen designing

---
+++ app/src/main/java/com/conduent/nationalhighways/utils/widgets/NHRadioGroup.kt
@@ -0,0 +1,11 @@
+package com.conduent.nationalhighways.utils.widgets
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RadioGroup
+
+class NHRadioGroup : RadioGroup{
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+}
