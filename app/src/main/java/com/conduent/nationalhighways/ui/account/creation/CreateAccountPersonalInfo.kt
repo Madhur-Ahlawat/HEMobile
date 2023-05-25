b9dc17a5e32086d3e54b04bf09ce8d98792fd477
omvir.singh <omvir.singh@hcl.com>
2023-05-25 15:09:59 +0530

    Error messages and country api have been integrated.

--- app/src/main/java/com/conduent/nationalhighways/ui/account/creation/CreateAccountPersonalInfo.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/account/creation/CreateAccountPersonalInfo.kt
@@ -2,11 +2,11 @@ package com.conduent.nationalhighways.ui.account.creation
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputFilter
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.conduent.nationalhighways.R
 import com.conduent.nationalhighways.data.model.account.AccountCreateRequestModel
@@ -16,6 +16,7 @@ import com.conduent.nationalhighways.ui.loader.OnRetryClickListener
 import com.conduent.nationalhighways.utils.common.Constants.IS_PERSONAL_ACCOUNT
 import com.conduent.nationalhighways.utils.extn.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
+
 
 const val accountRequestModelKey = "accountRequestModel"
 
@@ -32,9 +33,9 @@ class CreateAccountPersonalInfo : BaseFragment<FragmentCreateAccountPersonalInfo
         FragmentCreateAccountPersonalInfoNewBinding.inflate(inflater, container, false)
 
     override fun init() {
-        binding.inputFirstName.editText.addTextChangedListener(GenericTextWatcher(binding.inputFirstName.editText))
-        binding.inputLastName.editText.addTextChangedListener(GenericTextWatcher(binding.inputLastName.editText))
-        binding.inputCompanyName.editText.addTextChangedListener(GenericTextWatcher(binding.inputCompanyName.editText))
+        binding.inputFirstName.editText.addTextChangedListener(GenericTextWatcher(0))
+        binding.inputLastName.editText.addTextChangedListener(GenericTextWatcher(1))
+        binding.inputCompanyName.editText.addTextChangedListener(GenericTextWatcher(2))
 
         binding.btnNext.setOnClickListener(this)
         isPersonalAccount = arguments?.getBoolean(IS_PERSONAL_ACCOUNT,true)
@@ -42,6 +43,21 @@ class CreateAccountPersonalInfo : BaseFragment<FragmentCreateAccountPersonalInfo
             binding.txtCompanyName.visibility = View.GONE
             binding.inputCompanyName.visibility = View.GONE
         }
+        val filter = InputFilter { source, start, end, dest, dstart, dend ->
+            for (i in start until end) {
+                if (!Character.isLetterOrDigit(source[i]) &&
+                    Character.toString(source[i]) != "_" &&
+                    Character.toString(source[i]) != "-"
+                ) {
+                    return@InputFilter ""
+                }
+            }
+            null
+        }
+
+        binding.inputFirstName.editText.filters = arrayOf(filter)
+        binding.inputLastName.editText.filters = arrayOf(filter)
+        binding.inputCompanyName.editText.filters = arrayOf(filter)
     }
 
     override fun initCtrl() {
@@ -71,7 +87,7 @@ class CreateAccountPersonalInfo : BaseFragment<FragmentCreateAccountPersonalInfo
 
     }
 
-    inner class GenericTextWatcher(private val view: View) : TextWatcher {
+    inner class GenericTextWatcher(private val index: Int) : TextWatcher {
         override fun beforeTextChanged(
             charSequence: CharSequence?,
             start: Int,
@@ -85,16 +101,31 @@ class CreateAccountPersonalInfo : BaseFragment<FragmentCreateAccountPersonalInfo
             start: Int,
             before: Int,
             count: Int) {
-            if (binding.inputFirstName.getText()?.isNotEmpty() == true
-                && binding.inputLastName.getText()?.isNotEmpty() == true) {
+            if (binding.inputFirstName.getText()?.isNotEmpty() == true) {
                 requiredFirstName = true
-                requiredLastName = true
+                binding.inputFirstName.error = ""
             }else{
                 requiredFirstName = false
+                if(index == 0)
+                    binding.inputFirstName.error = "Enter the primary account-holder’s first name"
+            }
+            if (binding.inputLastName.getText()?.isNotEmpty() == true) {
+                requiredLastName = true
+                binding.inputLastName.error = ""
+            }else{
                 requiredLastName = false
+                if(index == 1)
+                    binding.inputLastName.error = "Enter the primary account-holder’s last name"
             }
             if(isPersonalAccount == false){
-                requiredCompanyName = binding.inputCompanyName.getText()?.isNotEmpty() == true
+                if (binding.inputCompanyName.getText()?.isNotEmpty() == true) {
+                    requiredCompanyName = true
+                    binding.inputCompanyName.error = ""
+                }else{
+                    requiredCompanyName = false
+                    if(index == 2)
+                        binding.inputCompanyName.error = "Enter the company name"
+                }
             }
 
 
