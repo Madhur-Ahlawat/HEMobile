7aeea2c590dd5a55296ce94fc23ee881b14eabcc
omvir.singh <omvir.singh@hcl.com>
2023-06-07 11:07:16 +0530

    create account summary updated

--- app/src/main/java/com/conduent/nationalhighways/ui/account/creation/newAccountCreation/CreateAccountSummaryFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/account/creation/newAccountCreation/CreateAccountSummaryFragment.kt
@@ -4,17 +4,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.conduent.nationalhighways.R
-import com.conduent.nationalhighways.databinding.CreateAccountSummaryBinding
+import com.conduent.nationalhighways.databinding.FragmentCreateAccountSummaryBinding
 import com.conduent.nationalhighways.ui.base.BaseFragment
 
 
-class CreateAccountSummaryFragment : BaseFragment<CreateAccountSummaryBinding>(),
+class CreateAccountSummaryFragment : BaseFragment<FragmentCreateAccountSummaryBinding>(),
     View.OnClickListener {
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): CreateAccountSummaryBinding= CreateAccountSummaryBinding.inflate(inflater,container,false)
+    ): FragmentCreateAccountSummaryBinding= FragmentCreateAccountSummaryBinding.inflate(inflater,container,false)
 
     override fun init() {
         binding.btnNext.setOnClickListener(this)
--- app/src/main/java/com/conduent/nationalhighways/ui/account/creation/newAccountCreation/VehicleListFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/account/creation/newAccountCreation/VehicleListFragment.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.conduent.nationalhighways.R
 import com.conduent.nationalhighways.databinding.FragmentVehicleList2Binding
 import com.conduent.nationalhighways.ui.account.creation.adapter.VehicleListAdapter
+import com.conduent.nationalhighways.ui.account.creation.new_account_creation.model.NewCreateAccountRequestModel
 import com.conduent.nationalhighways.ui.base.BaseFragment
 import com.conduent.nationalhighways.utils.common.Constants
 
@@ -16,6 +17,7 @@ class VehicleListFragment : BaseFragment<FragmentVehicleList2Binding>(),VehicleL
 
     private lateinit var vehicleList:ArrayList<String>
     private lateinit var vehicleAdapter:VehicleListAdapter
+    private var needMoreVehicle = false
 
 
     override fun getFragmentBinding(
@@ -27,12 +29,20 @@ class VehicleListFragment : BaseFragment<FragmentVehicleList2Binding>(),VehicleL
         vehicleList= ArrayList()
         vehicleList.add("s")
 
-
-
         binding.recyclerView.layoutManager=LinearLayoutManager(requireContext())
         vehicleAdapter= VehicleListAdapter(requireContext(), vehicleList,this)
         binding.recyclerView.adapter=vehicleAdapter
 
+        binding.radioGroupYesNo.setOnCheckedChangeListener { _, checkedId -> // checkedId is the RadioButton selected
+            when(checkedId){
+                R.id.radioButtonYes -> {
+                    needMoreVehicle = true
+                }
+                R.id.radioButtonNo -> {
+                    needMoreVehicle = false
+                }
+            }
+        }
 
     }
 
@@ -61,12 +71,14 @@ class VehicleListFragment : BaseFragment<FragmentVehicleList2Binding>(),VehicleL
         when(v?.id){
 
             R.id.btnNext->{
-                if (vehicleList.size>=5){
-                    findNavController().navigate(R.id.action_vehicleListFragment_to_maximumVehicleFragment)
-
+                if(needMoreVehicle) {
+                    if (vehicleList.size >= 5) {
+                        findNavController().navigate(R.id.action_vehicleListFragment_to_maximumVehicleFragment)
+                    } else {
+                        findNavController().navigate(R.id.action_vehicleListFragment_to_createAccountFindVehicleFragment)
+                    }
                 }else{
-                    findNavController().navigate(R.id.action_vehicleListFragment_to_createAccountFindVehicleFragment)
-
+                    findNavController().navigate(R.id.action_vehicleListFragment_to_createAccountSummaryFragment)
                 }
             }
         }
