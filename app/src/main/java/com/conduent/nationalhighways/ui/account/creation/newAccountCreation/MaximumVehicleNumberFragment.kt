76ebd9ef4c575508ab4731ef6b7f1a04ad607a5c
52364682 <shivam.gupta@conduent.com>
2023-06-09 17:11:02 +0530

    RUC vehicle add condition is added

--- app/src/main/java/com/conduent/nationalhighways/ui/account/creation/newAccountCreation/MaximumVehicleNumberFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/account/creation/newAccountCreation/MaximumVehicleNumberFragment.kt
@@ -7,23 +7,33 @@ import android.view.View
 import android.view.ViewGroup
 import com.conduent.nationalhighways.R
 import com.conduent.nationalhighways.databinding.FragmentMaximumVehicleNumberBinding
+import com.conduent.nationalhighways.ui.account.creation.new_account_creation.model.NewCreateAccountRequestModel
 import com.conduent.nationalhighways.ui.base.BaseFragment
 
 
 class MaximumVehicleNumberFragment : BaseFragment<FragmentMaximumVehicleNumberBinding>() {
 
 
-
-
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentMaximumVehicleNumberBinding= FragmentMaximumVehicleNumberBinding.inflate(inflater,container,false)
+    ): FragmentMaximumVehicleNumberBinding =
+        FragmentMaximumVehicleNumberBinding.inflate(inflater, container, false)
 
     override fun init() {
+        if (NewCreateAccountRequestModel.isExempted){
+            binding.textMaximumVehicle.text=getString(R.string.str_vehicle_exempt_detail_message,NewCreateAccountRequestModel.plateNumber)
+            binding.maximumVehicleAdded.text=getString(R.string.str_vehicle_exempt_message,NewCreateAccountRequestModel.plateNumber)
+            binding.cancelBtn.visibility=View.GONE
+        }
+
+        if (NewCreateAccountRequestModel.isRucEligible){
+
+        }
     }
 
     override fun initCtrl() {
+
     }
 
     override fun observer() {
