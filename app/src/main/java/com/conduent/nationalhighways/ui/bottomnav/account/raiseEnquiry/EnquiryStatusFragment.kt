1d3c4f7f89d8bc2179a33ea5d5298ad7e8286e91
madhur ahlawat <madhur.ahlawat@conduent.com>
2023-10-03 14:25:30 +0530

    WIP on new Landing screen flow

--- app/src/main/java/com/conduent/nationalhighways/ui/bottomnav/account/raiseEnquiry/EnquiryStatusFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/bottomnav/account/raiseEnquiry/EnquiryStatusFragment.kt
@@ -15,7 +15,6 @@ import com.conduent.nationalhighways.data.model.raiseEnquiry.EnquiryListResponse
 import com.conduent.nationalhighways.data.model.raiseEnquiry.EnquiryStatusRequest
 import com.conduent.nationalhighways.databinding.FragmentEnquiryStatusBinding
 import com.conduent.nationalhighways.ui.base.BaseFragment
-import com.conduent.nationalhighways.ui.bottomnav.HomeActivityMain
 import com.conduent.nationalhighways.ui.bottomnav.account.raiseEnquiry.viewModel.RaiseNewEnquiryViewModel
 import com.conduent.nationalhighways.ui.landing.LandingActivity
 import com.conduent.nationalhighways.ui.loader.LoaderDialog
@@ -39,6 +38,7 @@ class EnquiryStatusFragment : BaseFragment<FragmentEnquiryStatusBinding>() {
     var isViewCreated: Boolean = false
     var isApiCalled: Boolean = false
     var referenceNumberValidations: Boolean = false
+    var lastNameValidations: Boolean = false
 
     override fun getFragmentBinding(
         inflater: LayoutInflater,
@@ -47,9 +47,11 @@ class EnquiryStatusFragment : BaseFragment<FragmentEnquiryStatusBinding>() {
         FragmentEnquiryStatusBinding.inflate(inflater, container, false)
 
     override fun init() {
-        LandingActivity.setToolBarTitle("Enquiry Status")
-        LandingActivity.showToolBar(true)
+
         binding.enquiryReferenceNumberEt.editText.addTextChangedListener(GenericTextWatcher(1))
+        binding.lastNameEt.editText.addTextChangedListener(GenericTextWatcher(2))
+
+        binding.lastNameEt.editText.setText(viewModel.enquiry_last_name.value?:"")
         binding.enquiryReferenceNumberEt.editText.setText(viewModel.enquiry_status_number.value?:"")
         binding.btnNext.setOnClickListener {
             isApiCalled = false
@@ -64,17 +66,32 @@ class EnquiryStatusFragment : BaseFragment<FragmentEnquiryStatusBinding>() {
             )
 
         }
+
+        if(requireActivity() is RaiseEnquiryActivity){
+            binding.btnGotoStartMenu.visible()
+        }else{
+            binding.btnGotoStartMenu.gone()
+        }
+
+        binding.btnGotoStartMenu.setOnClickListener {
+            requireActivity().startNormalActivityWithFinish(
+                LandingActivity::class.java
+            )
+        }
     }
 
     override fun initCtrl() {
-        loader = LoaderDialog()
-        loader?.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.Dialog_NoTitle)
+
     }
 
     override fun observer() {
         if (!isViewCreated) {
             binding.viewModel = viewModel
             binding.lifecycleOwner = this
+
+            loader = LoaderDialog()
+            loader?.setStyle(DialogFragment.STYLE_NO_TITLE, R.style.Dialog_NoTitle)
+
             observe(viewModel.getAccountSRList, ::getAccountSRListResponse)
         }
         isViewCreated = true
@@ -117,7 +134,25 @@ class EnquiryStatusFragment : BaseFragment<FragmentEnquiryStatusBinding>() {
                         referenceNumberValidations = true
                     }
                 }
+            } else {
+                viewModel.enquiry_last_name.value = charSequence.toString()
+                if(charSequence.toString().trim().isEmpty()){
+                    lastNameValidations = false
+                    binding.lastNameEt.removeError()
+
+                }else  if (Utils.hasDigits(charSequence.toString()) || Utils.hasSpecialCharacters(
+                        charSequence.toString(),
+                        Utils.splCharVehicleMake
+                    )
+                ) {
+                    binding.lastNameEt.setErrorText(resources.getString(R.string.str_last_name_error_message))
+                    lastNameValidations = false
+                } else {
+                    binding.lastNameEt.removeError()
+                    lastNameValidations = true
+                }
             }
+
             checkButtonEnable()
         }
 
@@ -127,7 +162,7 @@ class EnquiryStatusFragment : BaseFragment<FragmentEnquiryStatusBinding>() {
     }
 
     private fun checkButtonEnable() {
-        if (referenceNumberValidations) {
+        if (referenceNumberValidations && lastNameValidations) {
             binding.btnNext.enable()
         } else {
             binding.btnNext.disable()
