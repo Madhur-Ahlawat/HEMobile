944bf23dab1473023f30d8124224c50f6442f76a
52364682 <shivam.gupta@conduent.com>
2023-10-11 10:50:20 +0530

    user story bug fixing

--- app/src/main/java/com/conduent/nationalhighways/ui/auth/suspended/AccountSuspendedFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/auth/suspended/AccountSuspendedFragment.kt
@@ -1,11 +1,9 @@
 package com.conduent.nationalhighways.ui.auth.suspended
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.conduent.nationalhighways.R
 import com.conduent.nationalhighways.data.model.account.PersonalInformation
