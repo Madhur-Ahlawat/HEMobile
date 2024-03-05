c6512ea73ca5b8df1b4b112b7d62c17ce1412779
Madhur Ahlawat <madhur.ahlawat@conduent.com>
2024-03-05 15:40:00 +0530

    Accessibility changes

--- app/src/main/java/com/conduent/nationalhighways/ui/auth/login/LoginActivity.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/auth/login/LoginActivity.kt
@@ -430,10 +430,6 @@ class LoginActivity : BaseActivity<FragmentLoginChangesBinding>(), View.OnClickL
             edtPwd.editText.doAfterTextChanged { passwordCheck() }
             btnLogin.setOnClickListener(this@LoginActivity)
             backButton.setOnClickListener(this@LoginActivity)
-            edtEmail.editText.setText("madhur.ahslawat@conduent.com")
-            edtPwd.editText.setText("Welcome2")
-            btnLogin.isEnabled = true
-            btnLogin.isClickable = true
         }
     }
 
