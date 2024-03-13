04972d228fdff3e50d9d6546d232e7cf6106d09e
Madhur Ahlawat <madhur.ahlawat@conduent.com>
2024-03-13 18:58:03 +0530

    Minor changes

--- app/src/main/java/com/conduent/nationalhighways/ui/bottomnav/account/raiseEnquiry/DartChargeAccountTypeEnquiryFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/bottomnav/account/raiseEnquiry/DartChargeAccountTypeEnquiryFragment.kt
@@ -61,23 +61,23 @@ class DartChargeAccountTypeEnquiryFragment :
         if(requireActivity() is RaiseEnquiryActivity){
             (requireActivity() as RaiseEnquiryActivity).focusToolBar()
         }
-        binding.radioButtonYes.setOnCheckedChangeListener { _, isChecked ->
-            binding.radioButtonYes.contentDescription = if (isChecked) {
-                "${binding.radioButtonYes.text}"
-            } else {
-                "${binding.radioButtonYes.text}"
-            }
-            binding.radioButtonYes.contentDescription = binding.radioButtonYes.text.toString()
-        }
-
-        binding.radioButtonNo.setOnCheckedChangeListener { _, isChecked ->
-            binding.radioButtonNo.contentDescription = if (isChecked) {
-                "${binding.radioButtonNo.text}"
-            } else {
-                "${binding.radioButtonNo.text}"
-            }
-            binding.radioButtonNo.contentDescription = binding.radioButtonNo.text.toString()
-        }
+//        binding.radioButtonYes.setOnCheckedChangeListener { _, isChecked ->
+//            binding.radioButtonYes.contentDescription = if (isChecked) {
+//                "Checked"
+//            } else {
+//                "Unchecked"
+//            }
+//            binding.radioButtonYes.contentDescription = binding.radioButtonYes.text.toString()
+//        }
+//
+//        binding.radioButtonNo.setOnCheckedChangeListener { _, isChecked ->
+//            binding.radioButtonNo.contentDescription = if (isChecked) {
+//                "Checked "+"${binding.radioButtonNo.text}"
+//            } else {
+//                "Unchecked "+"${binding.radioButtonNo.text}"
+//            }
+//            binding.radioButtonNo.contentDescription = binding.radioButtonNo.text.toString()
+//        }
     }
 
     private fun checkContinue() {
