d272e4e099d2fe05840d66b51ddae775bfb0f99f
52409756 <amrutha.swarna@conduent.com>
2024-06-13 18:16:11 +0530

    existingcardfragment changes

--- app/src/main/java/com/conduent/nationalhighways/ui/revalidatePayment/RevalidateExistingPaymentFragment.kt
+++ app/src/main/java/com/conduent/nationalhighways/ui/revalidatePayment/RevalidateExistingPaymentFragment.kt
@@ -1,18 +1,132 @@
 package com.conduent.nationalhighways.ui.revalidatePayment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.conduent.nationalhighways.R
-class RevalidateExistingPaymentFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_revalidate_existing_payment, container, false)
+import com.conduent.nationalhighways.data.model.account.payment.PaymentSuccessResponse
+import com.conduent.nationalhighways.data.model.manualtopup.PaymentWithExistingCardModel
+import com.conduent.nationalhighways.data.model.payment.CardListResponseModel
+import com.conduent.nationalhighways.data.model.payment.PaymentMethodDeleteResponseModel
+import com.conduent.nationalhighways.data.model.profile.PersonalInformation
+import com.conduent.nationalhighways.databinding.FragmentRevalidateExistingPaymentBinding
+import com.conduent.nationalhighways.ui.auth.suspended.ManualTopUpViewModel
+import com.conduent.nationalhighways.ui.base.BaseFragment
+import com.conduent.nationalhighways.utils.common.Constants
+import com.conduent.nationalhighways.utils.common.Resource
+import com.conduent.nationalhighways.utils.common.observe
+import com.google.gson.Gson
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class RevalidateExistingPaymentFragment : BaseFragment<FragmentRevalidateExistingPaymentBinding>() {
+    private var personalInformation: PersonalInformation? = null
+    private var paymentList: ArrayList<CardListResponseModel> = ArrayList()
+    private var position: Int = 0
+    private var paymentSuccessResponse: PaymentSuccessResponse? = null
+    private val manualTopUpViewModel: ManualTopUpViewModel by viewModels()
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentRevalidateExistingPaymentBinding =
+        FragmentRevalidateExistingPaymentBinding.inflate(inflater, container, false)
+
+    override fun init() {
+        if (arguments?.getParcelable<PersonalInformation>(Constants.PERSONALDATA) != null) {
+            personalInformation =
+                arguments?.getParcelable(Constants.PERSONALDATA)
+        }
+        val receivedList = arguments?.getParcelableArrayList<CardListResponseModel>(Constants.DATA)
+
+
+        if (receivedList != null) {
+            paymentList = receivedList
+        }
+
+        position = arguments?.getInt(Constants.POSITION, 0) ?: 0
+
+
+        if (arguments?.getParcelable<PaymentSuccessResponse>(Constants.NEW_CARD) != null) {
+            paymentSuccessResponse = arguments?.getParcelable(Constants.NEW_CARD)
+        }
     }
+
+    override fun initCtrl() {
+
+    }
+
+    override fun observer() {
+        lifecycleScope.launch {
+            observe(
+                manualTopUpViewModel.paymentWithExistingCard,
+                ::handlePaymentWithExistingCardResponse
+            )
+        }
+    }
+
+
+    private fun handlePaymentWithExistingCardResponse(status: Resource<PaymentMethodDeleteResponseModel?>?) {
+        dismissLoaderDialog()
+        when (status) {
+            is Resource.Success -> {
+
+            }
+
+            is Resource.DataError -> {
+                if (checkSessionExpiredOrServerError(status.errorModel)
+                ) {
+                    displaySessionExpireDialog(status.errorModel)
+                } else {
+                    if (status.errorModel?.message.equals("Something went wrong. Try again later")) {
+                    } else {
+//                        redirectToTryAgainPage()
+                    }
+                }
+            }
+
+            else -> {
+            }
+        }
+    }
+
+
+    private fun payWithExistingCard() {
+        val model = PaymentWithExistingCardModel(
+            addressline1 = personalInformation?.addressLine1.toString().replace(" ", ""),
+            addressline2 = personalInformation?.addressLine2.toString().replace(" ", ""),
+            transactionAmount = "",
+            cardType = "",
+            cardNumber = "",
+            cvv = "",
+            rowId = paymentList[position].rowId,
+            saveCard = "",
+            useAddressCheck = "N",
+            firstName = paymentList[position].firstName,
+            middleName = paymentList[position].middleName,
+            lastName = paymentList[position].lastName,
+            paymentType = "",
+            primaryCard = "",
+            maskedCardNumber = "",
+            easyPay = "",
+            cavv = paymentSuccessResponse?.cavv,
+            xid = paymentSuccessResponse?.xid,
+            threeDsVersion = paymentSuccessResponse?.threeDsVersion,
+            directoryServerId = paymentSuccessResponse?.directoryServerId,
+            cardHolderAuth = paymentSuccessResponse?.cardHolderAuth,
+            eci = paymentSuccessResponse?.eci
+
+
+        )
+        Log.d("paymentRequest", Gson().toJson(model))
+        manualTopUpViewModel.paymentWithExistingCard(model)
+    }
+
+
 
 }
